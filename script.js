bf6d0e2a2e39e06dba38806763858b6cc1bcff75
Meka Leela Venkatesh <mekaleelavenkatesh@gmail.com>
2022-12-28 21:54:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,12 @@ function buildCards(asArray=true){
     const values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
     const packArr = []
     const packObj = {}
+    for (var a in suits){
+       for(var s in values){
+        packArr.push(values[s]+"  of  "+suits[a])
+        packObj[values[s]+"  of  "+suits[a]] = parseInt(s) + 1
+       }
+    }
 
     // write your code here
 
